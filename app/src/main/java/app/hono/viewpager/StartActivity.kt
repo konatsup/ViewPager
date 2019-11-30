e7b572aab91eabccebe9ec1a8bbd1287192d17c3
konatsup <konatsukomatsu@gmail.com>
2019-11-30 20:43:28 +0900

    外部ストレージへのアクセスを許可するかのダイアログを表示

--- app/src/main/java/app/hono/viewpager/StartActivity.kt
+++ app/src/main/java/app/hono/viewpager/StartActivity.kt
@@ -1,16 +1,23 @@
 package app.hono.viewpager
 
-import android.app.Activity
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.activity_start.*
 import java.io.IOException
 import java.util.*
+import android.app.Activity as Activity
 
 class StartActivity : AppCompatActivity() {
 
@@ -27,11 +34,13 @@ class StartActivity : AppCompatActivity() {
 
     companion object {
         const val REQUEST_CODE_PHOTO: Int = 123
+        const val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        checkPermissionREAD_EXTERNAL_STORAGE(this)
 
         galleryButton.setOnClickListener {
             val intent: Intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
@@ -68,7 +77,85 @@ class StartActivity : AppCompatActivity() {
                 } catch (e: IOException) {
                     e.printStackTrace()
                 }
+
+    fun showDialog(
+        msg: String, context: Context,
+        permission: String
+    ) {
+        val alertBuilder = AlertDialog.Builder(context)
+        alertBuilder.setCancelable(true)
+        alertBuilder.setTitle("Permission necessary")
+        alertBuilder.setMessage("$msg permission is necessary")
+        alertBuilder.setPositiveButton(android.R.string.yes
+        ) { _, _ ->
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf(permission),
+                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+            )
+        }
+        val alert = alertBuilder.create()
+        alert.show()
+    }
+
+    fun checkPermissionREAD_EXTERNAL_STORAGE(
+        context: Context
+    ): Boolean {
+        val currentAPIVersion = Build.VERSION.SDK_INT
+        if (currentAPIVersion >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        context as Activity,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    showDialog(
+                        "External storage", context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+
+                } else {
+                    ActivityCompat
+                        .requestPermissions(
+                            context,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+                        )
+                }
+                return false
+            } else {
+                return true
             }
+
+        } else {
+            return true
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // do your stuff
+            } else {
+                Toast.makeText(
+                    applicationContext, "GET_ACCOUNTS Denied",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else -> super.onRequestPermissionsResult(
+                requestCode, permissions,
+                grantResults
+            )
+        }
+    }
+
+
 }
